f333a513222ec2adb571bc09584b1cd30ccabd1d
ArbatskayaNatalya <89268625+ArbatskayaNatalya@users.noreply.github.com>
2021-11-27 12:51:02 +0300

    autorisation after registration

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,6 @@ class UsersController < ApplicationController
 
   def new
     redirect_to root_url, alert: 'Вы уже залогинены' if current_user.present?
-
     @user = User.new
   end
 
@@ -18,10 +17,12 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
-      redirect_to root_url, notice: 'Пользователь успешно зарегестрирован!'
+      session[:user_id] = @user.id
+      redirect_to user_path(@user), notice: 'Пользователь успешно зарегистрирован!'
     else
       render 'new'
     end
+
   end
 
   def edit
