1d3389775aeb7da5491032ffc809e21f512e4de0
ArbatskayaNatalya <89268625+ArbatskayaNatalya@users.noreply.github.com>
2021-11-29 18:17:48 +0300

    add questions author

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -8,11 +8,12 @@ class QuestionsController < ApplicationController
   # POST /questions
   def create
     @question = Question.new(question_params)
+    @question.author_id = current_user.id
 
     if @question.save
       redirect_to user_path(@question.user), notice: 'Ваш вопрос задан'
     else
-      render :new
+      render :edit
     end
   end
 
